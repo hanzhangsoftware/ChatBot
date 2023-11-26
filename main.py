168849d02daba57a2e219489a88c75924296360f
hanzhangsoftware <137668170+hanzhangsoftware@users.noreply.github.com>
2023-11-25 21:43:41 -0500

    Update main.py
    new version of chatbot

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-curl https://api.openai.com/v1/chat/completions \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer $OPENAI_API_KEY" \
-  -d '{
-     "model": "gpt-3.5-turbo",
-     "messages": [{"role": "user", "content": "Say this is a test!"}],
-     "temperature": 0.7
-   }'
+import openai
+import gradio
 
+openai.api_key = "####"
+
+messages = [{"role": "system", "content": "You are a financial experts that specializes in real estate investment and negotiation"}]
+
+def CustomChatGPT(user_input):
+    messages.append({"role": "user", "content": user_input})
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = messages
+    )
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": ChatGPT_reply})
+    return ChatGPT_reply
+
+demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "Real Estate Pro")
+
+demo.launch(share=True)
